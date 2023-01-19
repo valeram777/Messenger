e6d4ecc5a13f53c953fc346e1d0f5b387045945b
valeram777 <vsevam2@gmail.com>
2023-01-19 17:18:57 +0300

    add firebase

---
+++ Firebase/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMuzC8CS0JPrgI8yx9Yh6QhUQoAOBUSLI",
+  authDomain: "clonewhatsapp-88218.firebaseapp.com",
+  projectId: "clonewhatsapp-88218",
+  storageBucket: "clonewhatsapp-88218.appspot.com",
+  messagingSenderId: "65056409746",
+  appId: "1:65056409746:web:ae43e34818db68d9217e4b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
